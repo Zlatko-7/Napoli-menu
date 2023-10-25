038751f8c0a948ef865d745e25f95707ab482520
Zlatko-7 <108333845+Zlatko-7@users.noreply.github.com>
2023-10-25 16:49:44 +0200

    Update data.js

--- data.js
+++ data.js
@@ -37,31 +37,31 @@ export const data = [
                 name: 'Cheeseburger',
                 price: '180 den',
                 ingredients: ['lepinja', 'mlako meso', 'cheddar sir', 'zelenchuk'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Bacon Burger',
                 price: '220 den',
                 ingredients: ['lepinja', 'mlako meso', 'cheddar sir', 'pekmez', 'zelenchuk'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Mushroom Burger',
                 price: '200 den',
                 ingredients: ['lepinja', 'mlako meso', 'cheddar sir', 'pecurki', 'zelenchuk'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Chicken Burger',
                 price: '190 den',
                 ingredients: ['lepinja', 'pileshko meso', 'majonez', 'zelena salata'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Veggie Burger',
                 price: '210 den',
                 ingredients: ['lepinja', 'zelenchuk', 'humus', 'avokado'],
-                image: ''
+                image: 'images/pizza2.png'
             }
         ],
         drinks: [
@@ -69,31 +69,31 @@ export const data = [
                 name: 'Coca-Cola',
                 price: '70 den',
                 size: '0.33L',
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Fanta',
                 price: '70 den',
                 size: '0.33L',
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Sprite',
                 price: '70 den',
                 size: '0.33L',
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Lemonade',
                 price: '80 den',
                 size: '0.5L',
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Iced Tea',
                 price: '90 den',
                 size: '0.5L',
-                image: ''
+                image: 'images/pizza2.png'
             }
         ],
         alcohol: [
@@ -101,31 +101,31 @@ export const data = [
                 name: 'Red Wine',
                 price: '300 den',
                 type: '750ml',
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'White Wine',
                 price: '280 den',
                 type: '750ml',
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Beer',
                 price: '150 den',
                 type: '500ml',
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Whiskey',
                 price: '500 den',
                 type: '50ml',
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Vodka',
                 price: '400 den',
                 type: '500ml',
-                image: ''
+                image: 'images/pizza2.png'
             }
         ],
         breakfast: [
@@ -133,31 +133,31 @@ export const data = [
                 name: 'Omelette',
                 price: '180 den',
                 ingredients: ['eggs', 'mushrooms', 'cheese', 'tomatoes'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Pancakes',
                 price: '150 den',
                 ingredients: ['flour', 'milk', 'eggs', 'butter', 'syrup'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Fruit Salad',
                 price: '200 den',
                 ingredients: ['strawberries', 'blueberries', 'bananas', 'apples', 'honey'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Croissant',
                 price: '100 den',
                 ingredients: ['flour', 'butter', 'sugar'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Yogurt Parfait',
                 price: '220 den',
                 ingredients: ['yogurt', 'granola', 'mixed berries', 'honey'],
-                image: ''
+                image: 'images/pizza2.png'
             }
         ],
         salads: [
@@ -165,31 +165,31 @@ export const data = [
                 name: 'Caesar Salad',
                 price: '220 den',
                 ingredients: ['romaine lettuce', 'croutons', 'parmesan', 'chicken', 'Caesar dressing'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Greek Salad',
                 price: '200 den',
                 ingredients: ['cucumbers', 'tomatoes', 'red onion', 'olives', 'feta cheese', 'olive oil'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Cobb Salad',
                 price: '250 den',
                 ingredients: ['avocado', 'bacon', 'chicken', 'eggs', 'tomatoes', 'blue cheese', 'vinaigrette'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Caprese Salad',
                 price: '240 den',
                 ingredients: ['mozzarella', 'tomatoes', 'basil', 'balsamic glaze'],
-                image: ''
+                image: 'images/pizza2.png'
             },
             {
                 name: 'Waldorf Salad',
                 price: '230 den',
                 ingredients: ['apples', 'celery', 'walnuts', 'grapes', 'mayonnaise'],
-                image: ''
+                image: 'images/pizza2.png'
             }
         ]
     }
